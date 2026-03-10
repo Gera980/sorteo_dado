126f80258f76a4d42a58900f3176a7bf787a5d7c
joseDomingoSH <sarabia2103@gmail.com>
2026-03-09 19:01:27 -0600

    Subir Proyecto de Probabilidad y Estadistica

---
+++ src/sorteo_dado/TestSorteoDado.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sorteo_dado;
+
+/**
+ *
+ * @author jSarabia
+ */
+public class TestSorteoDado {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
